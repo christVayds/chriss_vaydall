78777c695d939a537059edcc798a4a24b9f1301d
christVayds <christianvaydal112@gmail.com>
2025-02-17 11:49:27 +0800

    added image for mobile menu

--- Resources/scripts/index.js
+++ Resources/scripts/index.js
@@ -5,14 +5,18 @@ $(document).ready(function(){
     $('#open-mobile-nav').click(function(){
         if(open){
             document.getElementById('mobile-nav').classList.remove('show');
+            this.textContent = 'Menu';
             open = false;
         } else {
             document.getElementById('mobile-nav').classList.add('show');
+            this.textContent = 'Close';
             open = true;
         }
     });
     
     $('#mobile-nav').click(function(){
         this.classList.remove('show');
+        open = false;
+        document.getElementById('open-mobile-nav').textContent = 'Menu';
     });
 });
