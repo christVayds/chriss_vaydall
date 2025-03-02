7c728f8c9cdcfb5ca1f26f8ec3214daa1ed9616c
christVayds <christianvaydal112@gmail.com>
2025-03-02 22:03:30 +0800

    ...

--- Resources/scripts/service-workers.js
+++ Resources/scripts/service-workers.js
@@ -2,7 +2,7 @@
 self.addEventListener("install", (event) => {
     event.waitUntil(
         caches.open('v1').then((cache) => {
-            return cache.addAll(['/', 'index.html', 'Resources/styles/styles.css', 'Resources/scripts/index.js'])
+            return cache.addAll(['/', '/index.html', '/Resources/styles/index.css', '/Resources/scripts/index.js'])
         })
     );
 });
